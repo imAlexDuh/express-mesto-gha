7bae410b015c3ddf3121c4deeeed16ca6f6e2794
imAlexDuh <4uva40k99@gmail.com>
2023-10-22 22:00:01 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -46,9 +46,8 @@ const postUsers = (req, res) => {
 const updateUserProfile = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => {
-      if (!user) { return res.status(404).send({ message: 'Пользователь не найден' }); }
       res.status(200).send(user);
     })
     .catch(() => {
