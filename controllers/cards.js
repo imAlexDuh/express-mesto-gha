b2fba7764d71bf6f8134ecd52c583ad7a7ad0d5d
imAlexDuh <4uva40k99@gmail.com>
2023-10-22 23:01:44 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -67,7 +67,10 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((cards) => res.send({ cards }))
+  .then((cards) => {
+    if (!cards) { return res.status(404).send({ message: 'Такой карточки нет.' }); }
+    return res.send({ cards });
+  })
 
     .catch(() => {
       res.status(400).send({ message: `Переданы некорректные данные для постановки/снятии лайка.` });
