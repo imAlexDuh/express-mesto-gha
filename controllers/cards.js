b59491e4ba59cf2224cb479b67cdbc0b3302d750
imAlexDuh <4uva40k99@gmail.com>
2023-10-22 23:39:47 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -33,7 +33,7 @@ const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
   .then((cards) => {
     if (!cards) { return res.status(404).send({ message: 'Такой карточки нет.' }); }
-    return cards.remove().res.status(200).send(cards);
+    cards.remove().res.status(200).send(cards);
   })
 
     .catch(() => {
