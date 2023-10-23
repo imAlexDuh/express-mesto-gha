7dc6d7b7b1c8678923bf00b1298b8d49cda4a9aa
imAlexDuh <4uva40k99@gmail.com>
2023-10-23 23:25:53 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -20,10 +20,6 @@ const createCard = (req, res) => {
       res.status(201).send(card);
     })
 
-    .catch((err) => {
-      if (err.name === 'ValidationError') { return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' }); }
-      return res.status(500).send({ message: 'Внутренняя ошибка сервера' });
-    });
 }
 
 const deleteCard = (req, res) => {
