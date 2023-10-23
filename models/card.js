f8f1b267a776746c8d1571e79b414f9f923aacdf
imAlexDuh <4uva40k99@gmail.com>
2023-10-24 01:14:55 +0300

    Update card.js

--- models/card.js
+++ models/card.js
@@ -17,7 +17,7 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
   },
   likes: {
-    type: 'Schema.Types.ObjectId',
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     default: [],
   },
