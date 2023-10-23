153104d7aff60f62e081a02eec09dacca21533d0
imAlexDuh <4uva40k99@gmail.com>
2023-10-24 00:44:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cardsRoutes = require('./routes/cards');
