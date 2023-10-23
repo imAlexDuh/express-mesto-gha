a9c84838dffaba1ceac218880ca15a10bd8fe5b8
imAlexDuh <4uva40k99@gmail.com>
2023-10-23 23:43:15 +0300

    aboba

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,8 +15,8 @@ const createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner:req.user._id })
-    .then((card) => {
-      res.status(201).send(card);
+    .then((cards) => {
+      res.status(201).send(cards);
     })
 
     .catch((err) => {
