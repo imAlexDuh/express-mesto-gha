16c0015de6af947ddb9299e2d1df91b26ea9afa0
imAlexDuh <4uva40k99@gmail.com>
2023-10-23 22:33:27 +0300

    Update card.js

--- models/card.js
+++ models/card.js
@@ -12,7 +12,7 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
   owner: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: 'ObjectId',
     required: true
   },
   likes: {
