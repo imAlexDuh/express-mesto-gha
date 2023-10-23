7739fb381694fca7fd3823dcc48c402412e9a5ab
imAlexDuh <4uva40k99@gmail.com>
2023-10-24 00:11:54 +0300

    123

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,8 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
+{
+  "extends": "airbnb-base",
     "rules": {
-    }
+    "no-underscore-dangle": ["error", {
+      "allow": ["_id"]
+    }]
+  }
 }
