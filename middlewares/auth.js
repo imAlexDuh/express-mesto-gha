50209a850692524d787362a45eb7000ec0fef521
imAlexDuh <4uva40k99@gmail.com>
2023-11-04 00:26:18 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 const { SECRET } = require('../controllers/users');
+const BadAuthError = require('../errors/BadAuthError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new BadAuthError('Необходима авторизация.');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,9 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET);
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new BadAuthError('Необходима авторизация.');
   }
 
   req.user = payload;
