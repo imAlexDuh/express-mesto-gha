8509f505f31ebce83ccc8e310b33e260846ed8d9
imAlexDuh <4uva40k99@gmail.com>
2023-11-03 21:14:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,14 +18,17 @@ app.use(cookieParser());
 
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
-const { postUsers } = require('./controllers/users');
+const { postUsers, login } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
-app.use(auth);
-app.use('/', cardsRoutes);
-app.use('/', usersRoutes);
-app.post('/signin', usersRoutes);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -35,6 +38,10 @@ app.post('/signup', celebrate({
     avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
   }),
 }), postUsers);
+
+app.use(auth);
+app.use('/', cardsRoutes);
+app.use('/', usersRoutes);
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Такой страницы не существует' });
