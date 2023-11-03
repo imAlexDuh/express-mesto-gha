4bb5a174b38fe68cf122506b5b8ee0693417fda7
imAlexDuh <4uva40k99@gmail.com>
2023-11-03 23:21:48 +0300

    123123123

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,34 @@ const usersRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const validator = require('validator');
 
+const biba = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
+
 const {
   getUsers, getUserById, postUsers, updateUserProfile, patchMeAvatar, login, getCurrentUser,
 } = require('../controllers/users');
 
 usersRouter.get('/users', getUsers);
 usersRouter.get('/users/:id', getUserById);
-usersRouter.get('/signup', postUsers);
+
+usersRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) {
+        return value;
+      }
+      return helpers.message('Некорректный email');
+    }),
+    password: Joi.string().required(),
+    avatar: Joi.string().custom((value, helpers) => {
+      if (biba.test(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка');
+    }),
+  }),
+}), postUsers);
 
 usersRouter.post('/signin', celebrate({
   body: Joi.object().keys({
