aea60f57812ee1acfd4f48ec54486bbeaea897ec
imAlexDuh <4uva40k99@gmail.com>
2023-11-03 20:48:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const BodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 const auth = require('./middlewares/auth');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -14,8 +15,26 @@ app.use(express.json());
 
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
+const { postUsers, login } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+  }),
+}), postUsers);
 
 app.use(auth);
 app.use('/', cardsRoutes);
