233e3e2111a5d688be3134f26c54723a6e04e055
imAlexDuh <4uva40k99@gmail.com>
2023-11-03 21:03:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const BodyParser = require('body-parser');
-const auth = require('./middlewares/auth');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi } = require('celebrate');
+const auth = require('./middlewares/auth');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
+const { postUsers } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -24,7 +26,15 @@ app.use(auth);
 app.use('/', cardsRoutes);
 app.use('/', usersRoutes);
 app.post('/signin', usersRoutes);
-app.post('/signup', usersRoutes);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+  }),
+}), postUsers);
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Такой страницы не существует' });
