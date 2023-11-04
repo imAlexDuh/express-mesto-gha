d04910041537ef7d2031750bd6c6e9f53641e3de
imAlexDuh <4uva40k99@gmail.com>
2023-11-05 01:03:03 +0300

    123

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const BodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
-const auth = require('./middlewares/auth');
+// const auth = require('./middlewares/auth');
 const centralErrors = require('./middlewares/centralerrcontrol');
-const { login, postUsers } = require('./controllers/users');
 /* eslint-disable no-console */
 
 const app = express();
 const routes = require('./routes/routes');
-
-const ava = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 
 const { PORT = 3000 } = process.env;
 
@@ -27,48 +22,19 @@ async function run() {
   });
 }
 
-app.use(auth);
+// app.use(auth);
 app.use(BodyParser.json());
 app.use(express.json());
 app.use(errors());
 app.use(routes);
 
 const cardsRoutes = require('./routes/cards');
-const usersRoutes = require('./routes/users');
+const router = require('./routes/users');
 
 app.use('/', cardsRoutes);
-app.use('/', usersRoutes);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    email: Joi.string().required().custom((value, helpers) => {
-      if (validator.isEmail(value)) {
-        return value;
-      }
-      return helpers.message('Некорректный email');
-    }),
-    password: Joi.string().required(),
-    avatar: Joi.string().custom((value, helpers) => {
-      if (ava.test(value)) {
-        return value;
-      }
-      return helpers.message('Некорректная ссылка');
-    }),
-  }),
-}), postUsers);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().custom((value, helpers) => {
-      if (validator.isEmail(value)) {
-        return value;
-      }
-      return helpers.message('Некорректный email');
-    }),
-    password: Joi.string().required(),
-  }),
-}), login);
+app.use('/', router);
+app.post('/signin', router);
+app.post('/signup', router);
 
 app.use(centralErrors);
 
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,50 @@
-const usersRouter = require('express').Router();
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+
+const ava = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 
 const {
-  getUsers, getUserById, updateUserProfile, patchMeAvatar, getCurrentUser,
+  getUsers, getUserById, postUsers, updateUserProfile, patchMeAvatar, login, getCurrentUser,
 } = require('../controllers/users');
 
-usersRouter.get('/users', getUsers);
-usersRouter.get('/users/:id', getUserById);
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
 
-usersRouter.patch('/users/me', updateUserProfile);
-usersRouter.patch('/users/me/avatar', patchMeAvatar);
-usersRouter.get('/users/me', getCurrentUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) {
+        return value;
+      }
+      return helpers.message('Некорректный email');
+    }),
+    password: Joi.string().required(),
+    avatar: Joi.string().custom((value, helpers) => {
+      if (ava.test(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка');
+    }),
+  }),
+}), postUsers);
 
-module.exports = usersRouter;
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) {
+        return value;
+      }
+      return helpers.message('Некорректный email');
+    }),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.patch('/users/me', updateUserProfile);
+router.patch('/users/me/avatar', patchMeAvatar);
+router.get('/users/me', getCurrentUser);
+
+module.exports = router;
